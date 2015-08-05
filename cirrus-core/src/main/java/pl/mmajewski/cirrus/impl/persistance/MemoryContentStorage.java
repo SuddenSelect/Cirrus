a255d134c96a919d487e78117263b6f34771ff75
m168 <6865656+maciej168@users.noreply.github.com>
2015-08-05 17:38:34 +0200

    MemoryContentStorage - implementation without temporaries and maxes

--- cirrus-core/src/main/java/pl/mmajewski/cirrus/impl/persistance/MemoryContentStorage.java
+++ cirrus-core/src/main/java/pl/mmajewski/cirrus/impl/persistance/MemoryContentStorage.java
@@ -2,6 +2,7 @@ package pl.mmajewski.cirrus.impl.persistance;
 
 import pl.mmajewski.cirrus.common.model.ContentMetadata;
 import pl.mmajewski.cirrus.common.model.ContentPiece;
+import pl.mmajewski.cirrus.common.model.ContentStatus;
 import pl.mmajewski.cirrus.common.persistance.ContentStorage;
 
 import java.util.*;
@@ -9,29 +10,31 @@ import java.util.*;
 /**
  * Created by Maciej Majewski on 2015-02-03.
  */
-public class MemoryContentStorage implements ContentStorage{ //TODO implement
+public class MemoryContentStorage implements ContentStorage{ //TODO implement temporaries
     private Set<ContentMetadata> metadatas = new HashSet<>();
     private Map<String /*contentID*/,ContentMetadata> contentMetadataMap = new HashMap<>();
     private Map<String /*contentID*/,HashMap<Integer,ContentPiece>> piecesMap = new HashMap<>();
+    private int maxTemporaryPieces = Integer.MAX_VALUE;
+    private int maxStoredPieces = Integer.MAX_VALUE;
 
     @Override
     public void setMaxTemporarilyStoredPieces(int maxTemporaryPieces) {
-
+        this.maxTemporaryPieces =maxTemporaryPieces;
     }
 
     @Override
     public int getMaxTemporarilyStoredPieces() {
-        return 0;
+        return maxTemporaryPieces;
     }
 
     @Override
     public void setMaxStoredPieces(int maxPieces) {
-
+        this.maxStoredPieces = maxPieces;
     }
 
     @Override
     public int getMaxStoredPieces() {
-        return 0;
+        return maxStoredPieces;
     }
 
     @Override
@@ -56,15 +59,23 @@ public class MemoryContentStorage implements ContentStorage{ //TODO implement
     public void deleteContent(ContentMetadata metadata) {
         metadatas.remove(metadata);
         contentMetadataMap.remove(metadata.getContentId());
+        piecesMap.remove(metadata.getContentId());
     }
 
     @Override
     public Set<ContentMetadata> getCorruptedContentMetadata() {
-        return null;
+        Set<ContentMetadata> corrupted = new TreeSet<>();
+        for(ContentMetadata metadata : metadatas){
+            if(!getCorruptedContentPieces(metadata).isEmpty()){
+                corrupted.add(metadata);
+            }
+        }
+        return corrupted;
     }
 
     @Override
     public void storeContentPiece(ContentPiece contentPiece) {
+        //TODO handle maxStoredPieces
         if(!piecesMap.containsKey(contentPiece.getContentId())){
             piecesMap.put(contentPiece.getContentId(), new HashMap<>());
         }
@@ -73,27 +84,47 @@ public class MemoryContentStorage implements ContentStorage{ //TODO implement
 
     @Override
     public void storeContentPieceTemporarily(ContentPiece contentPiece) {
-
+        //TODO handle maxTemporaryPieces
     }
 
     @Override
     public void deleteContentPiece(ContentPiece contentPiece) {
-
+        HashMap<Integer,ContentPiece> pieces = piecesMap.get(contentPiece.getContentId());
+        if(pieces!=null) {
+            pieces.remove(contentPiece.getSequence());
+        }
     }
 
     @Override
     public Set<ContentPiece> getCorruptedContentPieces(ContentMetadata contentMetadata) {
-        return null;
+        Set<ContentPiece> corrupted = new TreeSet<>();
+        for(ContentPiece piece : piecesMap.get(contentMetadata.getContentId()).values()){
+            if(piece.getStatus().equals(ContentStatus.CORRUPTED)){
+                corrupted.add(piece);
+            }
+        }
+        return corrupted;
     }
 
     @Override
     public Set<ContentPiece> getCorruptedContentPieces() {
-        return null;
+        Set<ContentPiece> corrupted = new TreeSet<>();
+        for(ContentMetadata metadata : metadatas){
+            corrupted.addAll(getCorruptedContentPieces(metadata));
+        }
+        return corrupted;
     }
 
     @Override
     public Set<Integer> getMissingContentPieceSequenceNumbers(ContentMetadata contentMetadata) {
-        return null;
+        Set<Integer> missing = new TreeSet<>();
+        HashMap<Integer, ContentPiece> presentPieces = piecesMap.get(contentMetadata.getContentId());
+        for (int i = 0; i < contentMetadata.getPiecesAmount(); i++) {
+            if(!presentPieces.keySet().contains(i)){
+                missing.add(i);
+            }
+        }
+        return missing;
     }
 
     @Override
