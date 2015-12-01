bc2e947974e6150802a58cb7526ba804dc4bf40f
m168 <6865656+maciej168@users.noreply.github.com>
2015-12-01 21:31:43 +0100

    GUI - ContentAvailabilityPanel

---
+++ cirrus-gui/src/main/java/pl/mmajewski/cirrus/gui/model/ContentAvailabilityPanel.java
@@ -0,0 +1,34 @@
+package pl.mmajewski.cirrus.gui.model;
+
+import pl.mmajewski.cirrus.common.model.ContentAvailability;
+
+import javax.swing.*;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by Maciej Majewski on 01/12/15.
+ */
+public class ContentAvailabilityPanel {
+    private JPanel contentAvailabilityPanel;
+    private JTextField cirrusIdTextField;
+    private JTextField contentIdTextField;
+    private JList sequenceNumbersList;
+
+    private ContentAvailability contentAvailability = null;
+
+    synchronized public void apply(ContentAvailability contentAvailability){
+        this.contentAvailability = contentAvailability;
+
+        cirrusIdTextField.setText(contentAvailability.getHolderCirrusId());
+        contentIdTextField.setText(contentAvailability.getContentId());
+        Set numbers = new TreeSet<>(contentAvailability.getPiecesSequenceNumbers());
+        sequenceNumbersList.setListData(numbers.toArray());
+    }
+
+    public void refresh(){
+        if(contentAvailability!=null){
+            apply(contentAvailability);
+        }
+    }
+}
